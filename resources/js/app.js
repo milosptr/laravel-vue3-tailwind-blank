d103df24793eaff6dab47d7a3e3c866319c6370e
milospetrovic <milosp@ultimatemovers.net>
2021-07-03 14:47:32 +0200

    setup

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+// resources/js/app.js
+require("./bootstrap");
+
+import { createApp } from "vue";
+import App from "./components/App.vue";
+
+const app = createApp({
+  components: {
+    App
+  }
+});
+
+app.mount("#app");
